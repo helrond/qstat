aa7a69f4bf61b93e37e3c86a6d490aba7f390b20
helrond <helrond@hotmail.com>
2014-01-27 09:20:26 -0500

    adding teams and games list views

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,21 +8,27 @@ var qStat = angular.module('qStat.controllers', []);
 
   }]);
   
-  qStat.controller('gameCtrl', [function() {
-
-  }]);
+qStat.controller('gameListCtrl', [function() {
+	$http.get('games/games.json').success(function(data) {
+		$scope.games = data; 
+		});
+	$scope.orderProp = 'name';
+}]);
   
-qStat.controller('playerCtrl', function($scope, $http) {
+qStat.controller('playerListCtrl', function($scope, $http) {
 	$http.get('players/players.json').success(function(data) {
 		$scope.players = data; 
 		});
 	$scope.orderProp = 'name';
 });
   
-  qStat.controller('statCtrl', [function() {
+qStat.controller('statCtrl', [function() {
 
-  }]);
+}]);
   
-  qStat.controller('teamCtrl', [function() {
-
-  }]);
+qStat.controller('teamListCtrl', [function() {
+	$http.get('teams/teams.json').success(function(data) {
+		$scope.teams = data; 
+		});
+	$scope.orderProp = 'name';
+}]);
