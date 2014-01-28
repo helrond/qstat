d5a589147eea99eb1f2e58aee8e23701aff3ceea
helrond <helrond@hotmail.com>
2014-01-28 09:21:56 -0500

    making editable

--- app/js/directives.js
+++ app/js/directives.js
@@ -8,4 +8,47 @@ angular.module('qStat.directives', []).
     return function(scope, elm, attrs) {
       elm.text(version);
     };
-  }]);
+  }]).
+  directive("clickToEdit", function() {
+    var editorTemplate = '<div class="click-to-edit">' +
+        '<div ng-hide="view.editorEnabled">' +
+            '{{value}} ' +
+            '<a href="#" ng-click="enableEditor()"><span class="glyphicon glyphicon-edit"></span></a>' +
+        '</div>' +
+        '<div ng-show="view.editorEnabled">' +
+            '<input ng-model="view.editableValue">' +
+            '<a href="#" ng-click="save()"><span class="glyphicon glyphicon-ok"></span></a>' +
+            ' or ' +
+            '<a href="#" ng-click="disableEditor()"><span class="glyphicon glyphicon-remove"></span></a>.' +
+        '</div>' +
+    '</div>';
+
+    return {
+        restrict: "A",
+        replace: true,
+        template: editorTemplate,
+        scope: {
+            value: "=clickToEdit",
+        },
+        controller: function($scope) {
+            $scope.view = {
+                editableValue: $scope.value,
+                editorEnabled: false
+            };
+
+            $scope.enableEditor = function() {
+                $scope.view.editorEnabled = true;
+                $scope.view.editableValue = $scope.value;
+            };
+
+            $scope.disableEditor = function() {
+                $scope.view.editorEnabled = false;
+            };
+
+            $scope.save = function() {
+                $scope.value = $scope.view.editableValue;
+                $scope.disableEditor();
+            };
+        }
+    };
+});
