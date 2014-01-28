0a0180432ee445cd3db16eac70051433627ae161
helrond <helrond@hotmail.com>
2014-01-27 19:50:34 -0500

    working on routes

--- app/js/app.js
+++ app/js/app.js
@@ -12,15 +12,15 @@ angular.module('qStat', [
 config(['$routeProvider', 
     function($routeProvider) {
         $routeProvider.
-        when('/', {templateUrl: 'partials/home.html', controller: 'homeCtrl'});
-  .when('/games', {templateUrl: 'partials/games/list.html', controller: 'gameCtrl'});
-  .when('/games/:gameId', {templateUrl: 'partials/games/detail.html', controller: 'gameDetailCtrl'});
-  .when('/teams', {templateUrl: 'partials/teams/list.html', controller: 'teamCtrl'});
-  .when('/teams/:id', {templateUrl: 'partials/teams/detail.html', controller: 'teamDetailCtrl'});
-  .when('/players', {templateUrl: 'partials/players/list.html', controller: 'playerCtrl'});
-  .when('/players/:playerId', {templateUrl: 'partials/players/detail.html', controller: 'playerDetailCtrl'});
-  .when('/stats/select', {templateUrl: 'partials/stats/gameSelect.html', controller: 'statsCtrl'});
-  .when('/stats/start', {templateUrl: 'partials/stats/gameStart.html', controller: 'statsCtrl'});
-  .when('/stats/record', {templateUrl: 'partials/stats/record.html', controller: 'statsCtrl'});
+        when('/', {templateUrl: 'partials/home.html', controller: 'homeCtrl'})
+  .when('/games', {templateUrl: 'partials/games/list.html', controller: 'gameCtrl'})
+  .when('/games/:gameId', {templateUrl: 'partials/games/detail.html', controller: 'gameDetailCtrl'})
+  .when('/teams', {templateUrl: 'partials/teams/list.html', controller: 'teamCtrl'})
+  .when('/teams/:id', {templateUrl: 'partials/teams/detail.html', controller: 'teamDetailCtrl'})
+  .when('/players', {templateUrl: 'partials/players/list.html', controller: 'playerCtrl'})
+  .when('/players/:playerId', {templateUrl: 'partials/players/detail.html', controller: 'playerDetailCtrl'})
+  .when('/stats/select', {templateUrl: 'partials/stats/gameSelect.html', controller: 'statsCtrl'})
+  .when('/stats/start', {templateUrl: 'partials/stats/gameStart.html', controller: 'statsCtrl'})
+  .when('/stats/record', {templateUrl: 'partials/stats/record.html', controller: 'statsCtrl'})
   .otherwise({redirectTo: '/'});
 }]);
