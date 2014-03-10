ddc53b5db32f53553682bc92b6683ef99f53b8d2
helrond <helrond@hotmail.com>
2014-03-09 20:33:13 -0400

    adding login functionality

--- app/js/app.js
+++ app/js/app.js
@@ -8,39 +8,42 @@ angular.module('qStat',[
 'qStat.services',
 'qStat.directives',
 'qStat.controllers',
-'xeditable']).
+'xeditable',
+'UserApp']).
 config([ '$routeProvider',
 function ($routeProvider) {
     $routeProvider.
     when('/', {
-        templateUrl: 'partials/home.html', controller: 'homeCtrl'
+        templateUrl: 'partials/home.html', controller: 'homeCtrl', public:true
     }).when('/games', {
-        templateUrl: 'partials/games/list.html', controller: 'gameCtrl'
+        templateUrl: 'partials/games/list.html', controller: 'gameCtrl', public:true
     }).when('/games/new', {
-        templateUrl: 'partials/games/new.html', controller: 'gameDetailCtrl'
+        templateUrl: 'partials/games/new.html', controller: 'gameDetailCtrl', hasPermission:'games'
     }).when('/games/:gameId', {
-        templateUrl: 'partials/games/detail.html', controller: 'gameDetailCtrl'
+        templateUrl: 'partials/games/detail.html', controller: 'gameDetailCtrl', public:true
     }).when('/teams', {
-        templateUrl: 'partials/teams/list.html', controller: 'teamCtrl'
+        templateUrl: 'partials/teams/list.html', controller: 'teamCtrl', public:true
     }).when('/teams/new', {
-        templateUrl: 'partials/teams/new.html', controller: 'teamDetailCtrl'
+        templateUrl: 'partials/teams/new.html', controller: 'teamDetailCtrl', hasPermission:'teams'
     }).when('/teams/:teamId', {
-        templateUrl: 'partials/teams/detail.html', controller: 'teamDetailCtrl'
+        templateUrl: 'partials/teams/detail.html', controller: 'teamDetailCtrl', public:true
     }).when('/players', {
-        templateUrl: 'partials/players/list.html', controller: 'playerCtrl'
+        templateUrl: 'partials/players/list.html', controller: 'playerCtrl', public:true
     }).when('/players/new', {
-        templateUrl: 'partials/players/new.html', controller: 'playerDetailCtrl'
+        templateUrl: 'partials/players/new.html', controller: 'playerDetailCtrl', hasPermission:'players'
     }).when('/players/:playerId', {
-        templateUrl: 'partials/players/detail.html', controller: 'playerDetailCtrl'
+        templateUrl: 'partials/players/detail.html', controller: 'playerDetailCtrl', public:true
     }).when('/stats', {
-        templateUrl: 'partials/stats/stats.html', controller: 'statCtrl'
+        templateUrl: 'partials/stats/stats.html', controller: 'statCtrl', hasPermission:'stats'
     }).when('/login', {
-        templateUrl: 'partials/users/login.html', controller: 'userCtrl'
+        templateUrl: 'partials/users/login.html', controller: 'userCtrl', public:true, login:true
     }).when('/users/:userId', {
-        templateUrl: 'partials/users/detail.html', controller: 'userDetailCtrl'
+        templateUrl: 'partials/users/detail.html', controller: 'userDetailCtrl', public:true
     }).otherwise({
         redirectTo: '/'
     });
 }]).run(function (editableOptions) {
     editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
-});;
+}).run(function($rootScope, user) {
+    user.init({ appId: '531cec29e556d' });
+});
